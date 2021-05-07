163ba72ac61c7cc1bae81d9f456e96cb987a4d53
Franco Arratia <franck_ral@hotmail.com>
2021-05-07 12:18:20 -0400

    java-exercises-samples: Counting the number of repetitions of a word in a given string

--- java-exercises-samples/src/main/java/com/fral/extreme/algorithms/Exercise2.java
+++ java-exercises-samples/src/main/java/com/fral/extreme/algorithms/Exercise2.java
@@ -1,9 +1,10 @@
 package com.fral.extreme.algorithms;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 /*
  * To execute Java, please define "static void main" on a class
@@ -13,16 +14,27 @@ import java.util.Map;
  */
 public class Exercise2 {
 
+    private static final String SPACE = " ";
+
     public static void main(String[] args) {
         // Count the each work in the given string
         String input = "string this is a test string that contains many string a string strings";
 
         // Running approach 1
-        approach1(input);
+        // approach1(input);
+
+        // Running approach 2
+        // approach2(input);
+
+        // Running approach 2
+        // approach3(input);
+
+        // Running approach 2
+        approach4Sorted(input);
     }
 
     private static void approach1(String input) {
-        List<String> inputList = Arrays.asList(input.split(" "));
+        String[] inputList = input.split(SPACE);
 
         Map<String, Integer> result = new HashMap<>();
         for (String str : inputList) {
@@ -39,5 +51,39 @@ public class Exercise2 {
         result.forEach((key, value) -> System.out.println(String.format("Word [%s] - repeated [%s] times.", key, value)));
     }
 
-    private static void approach2(String input) {}
+    private static void approach2(String input) {
+        Map<String, Long> collect = Arrays.stream(input.split(SPACE))
+                .collect(groupingBy(Function.identity(), counting()));
+
+        collect.forEach((key, value) -> System.out.println(String.format("Word [%s] - repeated [%s] times.", key, value)));
+    }
+
+    private static void approach3(String input) {
+        Map<String, Integer> collect = Arrays.stream(input.split(SPACE))
+                .collect(groupingBy(Function.identity(), summingInt(e -> 1)));
+
+        collect.forEach((key, value) -> System.out.println(String.format("Word [%s] - repeated [%s] times.", key, value)));
+    }
+
+    private static void approach4Sorted(String input) {
+        Map<String, Long> collect = Arrays.stream(input.split(SPACE))
+                .collect(groupingBy(Function.identity(), counting()));
+
+        Map<String, Long> countByWordSorted = collect.entrySet()
+                .stream()
+                //.sorted(Map.Entry.comparingByKey())
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(
+                    Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (v1, v2) -> {
+                            throw new IllegalStateException();
+                        },
+                        LinkedHashMap::new
+                    )
+                );
+
+        countByWordSorted.forEach((key, value) -> System.out.println(String.format("Word [%s] - repeated [%s] times.", key, value)));
+    }
 }
