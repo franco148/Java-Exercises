192a4fbe0d9866f023a1b37394c4e8590ec2d982
Franco Arratia <franck_ral@hotmail.com>
2023-01-25 16:42:48 -0400

    ExpenseTrackerV1: Initial state of the project

---
+++ java-expense-tracker-v1/src/main/java/com/francofral/etracker/domain/ExpenseItem.java
@@ -0,0 +1,4 @@
+package com.francofral.etracker.domain;
+
+public class ExpenseItem {
+}
---
+++ java-expense-tracker-v1/src/test/java/com/francofral/etracker/JavaExpenseTrackerV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francofral.etracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExpenseTrackerV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
