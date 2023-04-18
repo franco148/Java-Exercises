67d2fedbd71b4c7ade1f8e04ce3bf9813c786a69
Franco Arratia <franck_ral@hotmail.com>
2023-04-18 19:59:51 -0400

    expense-tracker-v1: Updating the DTO for returning the expense information

--- java-expense-tracker-v1/src/main/java/com/francofral/etracker/dto/ExpenseByGroupCriteria.java
+++ java-expense-tracker-v1/src/main/java/com/francofral/etracker/dto/ExpenseByGroupCriteria.java
@@ -3,11 +3,17 @@ package com.francofral.etracker.dto;
 import com.francofral.etracker.domain.GroupBy;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 public interface ExpenseByGroupCriteria {
 
     Long getCategoryId();
     String getCategoryName();
+
+    Long getExpenseId();
+    LocalDateTime getExpenseDateTime();
+    String getExpenseName();
+    String getExpenseDetails();
     BigDecimal getTotalExpenses();
 
     GroupBy getGroupCriteria();
