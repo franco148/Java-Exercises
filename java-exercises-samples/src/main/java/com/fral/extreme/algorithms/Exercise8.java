486c8e60df7e409504bb64fa006a595d3fe6099f
franco.fral <franck_ral@hotmail.com>
2025-04-03 22:31:15 -0400

    Resolving a code challenge

--- java-exercises-samples/src/main/java/com/fral/extreme/algorithms/Exercise8.java
+++ java-exercises-samples/src/main/java/com/fral/extreme/algorithms/Exercise8.java
@@ -1,6 +1,8 @@
 package com.fral.extreme.algorithms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
@@ -26,15 +28,57 @@ public class Exercise8 {
      */
     public static void main(String[] args) {
 
+        // Scenario 1
+        String str1 = "xyz";
+        int k1 = 2;
+
+        // Scenario 1
+        String str2 = "xyz";
+        int k2 = 3;
+
+        // Scenario 1
+        String str3 = "xyz";
+        int k3 = 4;
+
+        // Scenario 1
+        String str4 = "xyz";
+        int k4 = 5;
+
+        // Scenario 1
+        String str5 = "xyz";
+        int k5 = 6;
+
+        shiftTheWords(str1, k1);
+        shiftTheWords(str2, k2);
+        shiftTheWords(str3, k3);
+        shiftTheWords(str4, k4);
+        shiftTheWords(str5, k5);
+
     }
 
-    private Set<Character> getAllOrderedAlphabetWords() {
+    private static void shiftTheWords(String str, int constant) {
+
+        List<Character> allAlphabetWords = new ArrayList<>(getAllOrderedAlphabetWords());
+
+        StringBuilder builder = new StringBuilder();
+        char[] stringsArray = str.toCharArray();
+        for (char letter : stringsArray) {
+            int currentPosition = getActualPositionOfTheCharacter(letter);
+            int nextPosition = getNextPositionOfTheCharacter(currentPosition, constant);
+
+            builder.append(allAlphabetWords.get(nextPosition));
+        }
+
+        System.out.println(String.format("[%s, %d] => %s", str, constant, builder.toString()));
+    }
+
+    private static Set<Character> getAllOrderedAlphabetWords() {
         return Stream.of('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
                          'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
                          'u', 'v', 'w', 'x', 'y', 'z').collect(Collectors.toSet());
     }
 
-    private int getActualPositionOfTheCharacter(char character) {
+    private static int getActualPositionOfTheCharacter(char character) {
         Set<Character> allAlphabetWords = getAllOrderedAlphabetWords();
 
         int index = 0;
@@ -48,7 +92,13 @@ public class Exercise8 {
         return index;
     }
 
-    private int getNextPositionOfTheCharacter(int currentPosition, int constant) {
+    private static int getNextPositionOfTheCharacter(int currentPosition, int constant) {
+        int alphabetSize = getAllOrderedAlphabetWords().size();
+        int position = currentPosition + constant;
+        if (position < alphabetSize) {
+            return position;
+        }
+
         return Math.abs(currentPosition + constant - getAllOrderedAlphabetWords().size());
     }
 }
