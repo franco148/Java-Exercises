1cc74b7ff2a70b92f38b606c2facba3c91420075
franco.fral <franck_ral@hotmail.com>
2025-05-08 20:31:17 -0400

    Questions: Adding some ramdom java questions

--- java-exercises-samples/src/main/java/com/fral/extreme/ramdom/questions/Question004.java
+++ java-exercises-samples/src/main/java/com/fral/extreme/ramdom/questions/Question004.java
@@ -16,10 +16,10 @@ package com.fral.extreme.ramdom.questions;
  *
  * 2. Which of the following are benefits of Front Controller Pattern?
  *
- * a. Avoids duplicate control logic
+ * a. Avoids duplicate control logic                        x
  * b. Avoids embedding program logic in view
  * c. Uses template-based views
- * d. Applies common logic to multiple requests
+ * d. Applies common logic to multiple requests             x
  *
  *
  *
@@ -39,22 +39,169 @@ package com.fral.extreme.ramdom.questions;
  * Carmi successfully decrypts the jar file using the private key. Which of the following statements regarding
  * this scenario is true?
  *
- * a. The jar file may contain malicious code
+ * a. The jar file may contain malicious code                           x
  * b. Julio Cesar has written the jar file
  * c. The jar file may not have been signed with Carmi's public key
  * d. The code has not been modified in transit.
  *
  *
  *
+ * Which terminal operations on the Stream class are reductions? (Choose all that apply)
+ *
+ * A. collect()         x
+ * B. count()           x
+ * C. findFirst()
+ * D. map()
+ * E. peek()
+ * F. sum()             x
+ *
+ *
+ * Which of the following events will cause a thread to die?
+ *
+ * A. The method sleep() is called.
+ * B. The method wait() is called.
+ * C. Execution of the start() method ends.
+ * D. Execution of the run() method ends                    x
+ * E. Execution of the run() method is called.
+ *
+ *
+ *
+ * In below java program, which exception will occur? FileReader file = new FileReader("test.txt");
+ *
+ * A. NullPointerException at compile time
+ * B. NullPointerException at runtime
+ * C. FileNotFoundException at compile time
+ * D. FileNotFoundException at runtime              x
+ *
+ *
+ *
+ * What is -Xms and -Xmx while starting the JVM?
+ *
+ * A. Maximum and initial memory
+ * B. Initial and maximum memory                            x
+ * C. Extra memory on start and extra memory extended
+ * D. They are not valid flags
+ *
+ *
+ * In kafka records are published to:
+ *
+ * A. Topic                 x
+ * B. Category
+ * C. Table
+ * D. Subject
+ *
+ *
+ *
+ * With the default settings of a kafka producer, which part of a record is used to determine its target partition
+ * within the topic.
+ *
+ * A. Record key                x
+ * B. Record value
+ * C. Header key
+ * D. Record timestamp
+ *
+ *
+ *
+ * Kafka. How is message ordering defined in kafka? Ordering is guaranteed...
+ *
+ * A. across all topics in a kafka cluster
+ * B. for all messages in a single topic from a single producer
+ * C. with the same key delivered by all producers writing to that partition        x
+ * D. for all messages with the same key delivered by a single producer.
+ * E. There are no message ordering guarantees in kafka
+ *
+ *
+ *
+ * Kafka. your consumer application has a real-time processing requirement. What information below is critical to monitor?
+ *
+ * A. Total time to serve requests to replica followers
+ * B. The difference between the current offset to be read and the log end offset               x
+ * C. Aggregate incoming byte rate
+ * D. Consumer heartbeat rate to the Group Coordinator.
+ *
+ *
+ *
+ * A collection and a document in MongoDB is equivalent to......... concepts respectively.
+ *
+ * A. Table and Column
+ * B. Table and Row             x
+ * C. Column and Row
+ * D. Database and Table
+ *
+ *
+ *
+ * In which format MongoDB represents document structure?
+ *
+ * A. BSON              x
+ * B. TXT
+ * C. DOCX
+ * D. None of these
  *
  *
  *
  *
+ * In an aggregation, the most basic pipeline stages provide............ that operate like queries.
+ *
+ * A. Methods
+ * B. Filters                           x
+ * C. Store procedure
+ * D. None of the mentioned
  *
  *
  *
  *
+ * You can't create multiple containers from the same image in docker.
  *
+ * A. True
+ * B. False                     x
+ *
+ *
+ *
+ *
+ * ........ Is the technology that lets you create useful IT services using resources that are traditionally bound to hardware.
+ *
+ * A. Virtualization                    x
+ * B. Docker hub
+ * C. Docker swarm
+ * D. All of the above
+ *
+ *
+ *
+ * In scrum, the sprint goal is (Choose the best answer):
+ *
+ * A. Recommended, but optional
+ * B. An artifact that creates transparency through inspection and adaptation
+ * C. An objective that provides flexibility and guidance to the Developers throughout the sprint   x
+ * D. Decided by the Scrum Master, in collaboration with the Product Owner.
+ *
+ *
+ *
+ * Which concept is NOT defined in the Scrum Framework?
+ *
+ * A. Scrum Master
+ * B. Project Manager               x
+ * C. Scrum Product Owner
+ * D. Daily Scrum
+ * E. Scrum product burndown
+ *
+ *
+ *
+ * Which metric is most appropriate for evaluating the performance of an API under high load?
+ *
+ * A. Number of requests per second                 x
+ * B. Number of registered users
+ * C. Percentage of code errors
+ * D. Dis space
+ *
+ *
+ *
+ *
+ * Which of the following alerts would be most useful for detecting frequent failures in an API?
+ *
+ * A. Alert for memory usage
+ * B. Alert for an increase in the number of commits
+ * C. Alert for high response time                          x
+ * D. Alert for low user activity
  *
  *
  *
